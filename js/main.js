54c11ba0d68e3e1915a2acb756bc6e359cc98fe6
OS-50101 <sapov@mozarteum.at>
2019-08-02 12:34:40 +0200

    inital

---
+++ js/main.js
@@ -0,0 +1,38 @@
+///////////////////////////
+/* Create the vrvToolkit */
+///////////////////////////
+var vrvToolkit = new verovio.toolkit();
+var meiData;
+
+
+////////////////////////////////////
+/* Load the file using a HTTP GET */
+////////////////////////////////////
+$.ajax({
+	url: "./data/MRE.xml", dataType: "text", success: function (data) {
+		svg = vrvToolkit.renderData(data, {
+		});
+		$("#rendering").html(svg);
+	}
+});
+
+$("#changeSize").on("click", function () {
+	$("#rendering").css("width", "2500px");
+	/*$("header").css("color", "blue");*/
+})
+
+
+/*function renderMEI() {
+options = {
+adjustPageHeight: true,
+scale: 25,
+noLayout: 1,
+appXPathQuery: appSelection,
+substXPathQuery: substSelection
+};
+vrvToolkit.setOptions(options);
+
+var svg = vrvToolkit.renderData(meiData, {
+});
+$("#rendering").html(svg);
+}*/
